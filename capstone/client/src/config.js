46f75b426753b8ded79bd3f48d24125946f99d69
Lashonda <lashondamiller007@gmail.com>
2020-09-10 16:15:59 -0400

    try this

--- capstone/client/src/config.js
+++ capstone/client/src/config.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
-export const BACKEND_URL= process.env.NODE_ENV === 'development'?"http://localhost:5000/": "https://updatedtravels.herokuapp.com/"
+export const ATLAS_URI= process.env.NODE_ENV === 'development'?"http://localhost:5000/": "https://updatedtravels.herokuapp.com/"
 // comment out this file and procfile and your imports for the config file if it doesn't build after this
 //this one works ignore top comment
