912add3e62ed5281b06340b860c462b975a42ea8
Lashonda <lashondamiller007@gmail.com>
2020-09-10 16:41:41 -0400

    work

--- capstone/client/src/config.js
+++ capstone/client/src/config.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
-
+// below is a ternary operator
 dotenv.config();
-export const ATLAS_URI= process.env.NODE_ENV === 'development'?"http://localhost:5000/": "https://updatedtravels.herokuapp.com/"
+export const BACKEND_URL= process.env.NODE_ENV === 'development'?"http://localhost:5000/": "https://updatedtravels.herokuapp.com/"
+
 // comment out this file and procfile and your imports for the config file if it doesn't build after this
-//this one works ignore top comment
+//this one works ignore top comment 
